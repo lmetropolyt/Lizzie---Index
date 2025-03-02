17ba94e856d1e313e60cf2ab0c6b86f16daff89f
lmetropolyt <lmetropolyt@ocadu.ca>
2025-03-01 19:54:37 -0500

    FINAL TOUCHES integration

--- CitingCode/sketch.js
+++ CitingCode/sketch.js
@@ -1,12 +1,13 @@
 //This code was learned from p5 Recursive Tree tutorial unless otherwise marked. The parts of the code i added was the growth action
-
+//i learned how to integrate the tree into the structure of a website. It was very hard to get it to fit in good 
 let angle;
 let branchLength = 0; 
 let maxBranchLength = 200; // modified to change height the tree would 
 let growthSpeed = 1; 
 
 function setup() {
-  createCanvas(windowWidth, 600); //Changed to fit any screen
+  let canvas = createCanvas(windowWidth, 600); //Changed to fit any screen
+  canvas.parent('treeCanvas'); // Attach the canvas to the div with id "treeCanvas" // Added
   colorMode(HSB);
   angleMode(DEGREES);
 }
