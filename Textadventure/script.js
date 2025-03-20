083b4464fb30736dc23a09e88ce2c0df61668a7d
lmetropolyt <lmetropolyt@ocadu.ca>
2025-03-20 15:49:06 -0400

    radio button added

--- Textadventure/script.js
+++ Textadventure/script.js
@@ -1,6 +1,7 @@
 var input;
 var button;
 var words = [];
+var animationType = 'boiling';
 
 function setup() {
   let canvas = createCanvas(300, 300);
@@ -9,6 +10,11 @@ function setup() {
   input = select('#nameInput');
   button = select('#submitButton');
   button.mousePressed(addWord);
+  
+  let radios = selectAll('input[name="animation"]');
+  for (let i = 0; i < radios.length; i++) {
+    radios[i].changed(changeAnimation);
+  }
   
   background(100);
   noStroke();
@@ -32,6 +38,10 @@ function addWord() {
   }
 }
 
+function changeAnimation() {
+  animationType = this.value();
+}
+
 class Word {
   constructor(name, x, y) {
     this.name = name;
@@ -42,13 +52,27 @@ class Word {
   }
 
   update() {
-    this.y += this.ySpeed;
-    this.x += this.xSpeed;
-    if (this.y > height) {
-      this.y = 0;
-    }
-    if (this.x > width || this.x < 0) {
-      this.xSpeed *= -1;
+    if (animationType === 'boiling') {
+      this.y += this.ySpeed;
+      this.x += this.xSpeed;
+      if (this.y > height) {
+        this.y = 0;
+      }
+      if (this.x > width || this.x < 0) {
+        this.xSpeed *= -1;
+      }
+    } else if (animationType === 'floating') {
+      this.y += sin(frameCount / 10) * 2;
+      this.x += cos(frameCount / 10) * 2;
+    } else if (animationType === 'bouncing') {
+      this.y += this.ySpeed;
+      this.x += this.xSpeed;
+      if (this.y > height || this.y < 0) {
+        this.ySpeed *= -1;
+      }
+      if (this.x > width || this.x < 0) {
+        this.xSpeed *= -1;
+      }
     }
   }
 
