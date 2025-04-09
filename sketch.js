2736eab2c54e4cf8ef03d914ddbddd4cd6399b9b
lmetropolyt <lmetropolyt@ocadu.ca>
2025-04-08 23:56:05 -0400

    create image bug fixed

--- sketch.js
+++ sketch.js
@@ -1,52 +1,42 @@
-// load image
-// load a video
-// load a sound
-// playback rate 
-// volume 
-// many kitties
-var kitten;
-var kindle;
-var meow;
-var numberofkitties = 9;
-function setup(){
-    createCanvas(windowWidth, windowHeight);
-    imageMode(CENTER);
-    kitten = loadImage('kitten.jpeg');
-    kindle = createVideo('kittens.mp4');
-    kindle.hide();
-    kindle.volume(0);
-    kindle.play();
-    meow = createAudio('meow.wav');
-    meow.speed(10);
-}
+var gradientImage;
 
-function draw(){
-    background('blue');
-    imageMode(CORNER);
-    for(var x = 0; x < numberofkitties; x++){
-        for(var y = 0; y < numberofkitties; y++){
-            push();
-            translate(x * width/numberofkitties, y * height/numberofkitties);
-            rotate(frameCount/50);
-            image(kindle, 0, 0, 100, 100);
-            pop();
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    // Create a gradient image using createImage()
+    gradientImage = createImage(width, height); // Match the canvas size
+    gradientImage.loadPixels();
+    for (let x = 0; x < gradientImage.width; x++) {
+        for (let y = 0; y < gradientImage.height; y++) {
+            let a = map(y, 0, gradientImage.height, 0, 255); // Vertical gradient
+            let c = color(255, 255, 255, a); // White gradient with transparency
+            gradientImage.set(x, y, c); // Set pixel color
         }
     }
-    imageMode(CENTER);
-    push();
-    translate(mouseX, mouseY);
-    rotate(frameCount/30);
-    image(kitten, 0, 0);
-    pop();
+    gradientImage.updatePixels(); // Apply changes to the image
 }
 
-function mousePressed(){
-    kindle.play();
-    meow.speed(10);
-    meow.play();
+function draw() {
+    // Rainbow background
+    let gradientSpeed = frameCount % 400; // Animate the gradient
+    background(lerpColor(color('red'), color('blue'), gradientSpeed / 400));
+
+    // Overlay the gradient image
+    image(gradientImage, 0, 0, width, height);
 }
 
-
-function windowResized(){
+function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
+
+    // Recreate the gradient image to match the new canvas size
+    gradientImage = createImage(width, height);
+    gradientImage.loadPixels();
+    for (let x = 0; x < gradientImage.width; x++) {
+        for (let y = 0; y < gradientImage.height; y++) {
+            let a = map(y, 0, gradientImage.height, 0, 255); // Vertical gradient
+            let c = color(255, 255, 255, a); // White gradient with transparency
+            gradientImage.set(x, y, c); // Set pixel color
+        }
+    }
+    gradientImage.updatePixels();
 }
